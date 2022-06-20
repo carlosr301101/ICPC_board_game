2e464258693115833a390a5adaa701f744f0e2a2
int_64 <34899128+carlosr301101@users.noreply.github.com>
2022-06-20 19:39:34 -0400

    Update counting_product.cpp

--- ICPC21_22/counting_product/counting_product.cpp
+++ ICPC21_22/counting_product/counting_product.cpp
@@ -2,24 +2,50 @@
 
 using namespace std;
 
-
 int main()
 {
     int n,k; scanf(" %d",&n); scanf(" %d",&k);
-    int x[k];
 
-    //llenamos el arreglo
-    for (int i = 0; i < k; i++)
+    if (k<n && k!=2)
     {
-        x[i]=0;
-    }
+        int contador=k;
+        long suma=0;
 
-    for (int i = 0; i < k-1; i++)
+        for (long i = k+1; i <= n; i++)
+        {
+            long numero=i;
+            
+            for(long j=2;numero>1;j++)
+            {
+                while(numero%j==0)
+                {
+                    suma+=j;
+                    numero /= j;
+
+                }
+            }
+            if (suma<=k)
+            {
+                contador++;
+            }
+            suma=0;
+        }
+       // if (contador==2)contador--;
+        
+        printf("%d", contador);
+        
+    }
+    else
+    if (k!=2)
     {
-       
-       
+        printf("%d",n);
     }
-
+    else
+    {
+        printf("%d",1);
+    }
+    
+   
     
     return 0;
 }
